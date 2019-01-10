64b00bb13bc16604d3d3a1eacaaab6bfbe6e05af
henryHTH <46570019+henryHTH@users.noreply.github.com>
2019-01-10 11:31:51 -0500

    Add files via upload

---
+++ Geeksforgeek_ChangeCoin.py
@@ -0,0 +1,18 @@
+def cc(coins, target):
+    if (len(coins), target) in memo:
+        return memo[(len(coins), target)]
+    if target == 0: return 1
+    if target < 0: return 0
+    if len(coins) == 0:
+        return 0
+    memo[(len(coins), target)] = cc(coins, target - coins[-1]) + cc(coins[:-1], target)
+    return memo[(len(coins), target)]
+
+
+if __name__ == '__main__':
+    for i in range(int(input())):
+        n = int(input())
+        coins = [int(x) for x in input().strip().split()]
+        target = int(input())
+        memo = {}
+        print(cc(coins, target))
